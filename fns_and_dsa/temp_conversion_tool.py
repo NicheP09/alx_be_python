5f6793062167de7896c27ae9be6f1296a067c6cd
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-23 17:54:46 +0100

    Refactor temp_conversion_tool.py, arithmetic_operations.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 
 def convert_to_celsius(fahrenheit):
@@ -11,8 +11,10 @@ def convert_to_fahrenheit(celsius):
 
 
 def main():
+    temp_input = input("Enter the temperature to convert: ")
+
     try:
-        temp = float(input("Enter the temperature to convert: "))
+        temp = float(temp_input)
     except ValueError:
         raise ValueError("Invalid temperature. Please enter a numeric value.")
 
