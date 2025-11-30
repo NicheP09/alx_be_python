e2240a7c68a80ed00aa74f39e1172a7b41014b20
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-30 22:20:53 +0100

    change name test multiplication

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -17,7 +17,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 10), -10)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test multiplication functionality."""
         self.assertEqual(self.calc.multiply(4, 3), 12)
         self.assertEqual(self.calc.multiply(-2, 5), -10)
